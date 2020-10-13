aefc05adc2a416069107a99f3f87ed1a7b733ac0
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-10-13 11:59:48 +0300

    Add some element based on what property it has

--- App.js
+++ App.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import Input from './Input'
+import Input from './Input';
+import "./input.css"
 
 function App() {
     return (
-        <Input />
+        <article>
+            <section>
+                {`<Input />`} 
+                <br/>
+                <Input />
+            </section>
+            <section>
+                {`<Input error />`} 
+                <br/>
+                <Input error />
+            </section>
+            <section>
+                {`<Input disabled />`} 
+                <br/>
+                <Input disabled />
+            </section>
+            <section>
+                {`<Input helperText="You can use as much words as you want" />`} 
+                <br/>
+                <Input helperText="You can use as much words as you want" />
+            </section>
+        </article>
     )
 }
 
