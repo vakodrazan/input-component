51e4ff5fe4d833a5883d6f5d3847beef7a925fc1
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-10-14 14:39:58 +0300

    Set some class name to style each element easily

--- App.js
+++ App.js
@@ -5,47 +5,50 @@ import "./input.css"
 function App() {
     return (
         <article>
+            <header>
+                <h2>Input field</h2>
+            </header>
             <section>
-                <div>
+                <div className="content">
                     {`<Input />`} 
                     <br/>
                     <Input />
                 </div>
             </section>
             <section>
-                <div>
+                <div className="content">
                     {`<Input error />`} 
                     <br/>
                     <Input error />
                 </div>
             </section>
             <section>
-                <div>
+                <div className="content">
                     {`<Input disabled />`} 
                     <br/>
                     <Input disabled />
                 </div>
             </section>
-            <section>
-                <div> 
+            <section className="wrapper">
+                <div className="content"> 
                     {`<Input helperText="You can use as much words as you want" />`} 
                     <br/>
                     <Input helperText="You can use as much words as you want" /> 
                 </div>
-                <div> 
+                <div className="content"> 
                     {`<Input helperText="You can use as much words as you want" error />`} 
                     <br/>
                     <Input helperText="You can use as much words as you want" error />
                 </div>
             </section>
 
-            <section>
-                <div> 
+            <section className="wrapper">
+                <div className="content"> 
                     {`<Input startIcon="call" />`} 
                     <br/>
                     <Input startIcon="call" />
                 </div>
-                <div> 
+                <div className="content"> 
                     {`<Input endIcon="lock" />`} 
                     <br/>
                     <Input endIcon="lock" />
@@ -53,7 +56,7 @@ function App() {
             </section>
 
             <section>
-                <div>
+                <div className="content">
                     {`<Input value="text" />`} 
                     <br/>
                     <Input value="text" />
@@ -61,13 +64,13 @@ function App() {
             </section>
 
             {/* Size */}
-            <section>
-                <div> 
+            <section className="wrapper">
+                <div className="content"> 
                     {`<Input size="sm" />`} 
                     <br/>
                     <Input size="sm" />
                 </div>
-                <div> 
+                <div className="content"> 
                     {`<Input size="md" />`} 
                     <br/>
                     <Input size="md" />
@@ -75,7 +78,7 @@ function App() {
             </section>
 
             <section>
-                <div>
+                <div className="content">
                     {`<Input fullWidth />`} 
                     <br/>
                     <Input fullWidth />
@@ -83,7 +86,7 @@ function App() {
             </section>
 
             <section>
-                <div>
+                <div className="content">
                     {`<Input multiline row="4" />`} 
                     <br/>
                     <Input multiline row="4" />
